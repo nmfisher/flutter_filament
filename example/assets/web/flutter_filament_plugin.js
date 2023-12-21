a5112bee712bb99d4a0a79da61d101fc272e6ae8
Nick Fisher <nick.fisher@avinium.com>
2023-12-21 16:26:10 +0800

    update web assets

--- example/assets/web/flutter_filament_plugin.js
+++ example/assets/web/flutter_filament_plugin.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a8990d924aa09bcef4371f39e8db829aee0c1bb6830029293bb082225f2175b3
-size 4132501
+oid sha256:2cdf9947b40b78479b6d1d4bbc73fa2a3e00692c24b079b5b585b2747fe1c5c3
+size 4134535
