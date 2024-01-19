d8525cb689204907a8666066733491b46da17e3b
Nick Fisher <nick.fisher@avinium.com>
2024-01-19 11:44:50 +0800

    remove wasm-specific hacks for web plugin and add initialization future

--- lib/FlutterFilamentPluginWeb.dart
+++ lib/FlutterFilamentPluginWeb.dart
@@ -3,79 +3,25 @@
 // package as the core of your plugin.
 // ignore: avoid_web_libraries_in_flutter
 
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/services.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:flutter_filament/ffi/ffi.dart';
 
-// @AbiSpecificIntegerMapping({
-//   Abi.androidArm: Uint8(),
-//   Abi.androidArm64: Uint8(),
-//   Abi.androidIA32: Int8(),
-//   Abi.androidX64: Int8(),
-//   Abi.androidRiscv64: Uint8(),
-//   Abi.fuchsiaArm64: Uint8(),
-//   Abi.fuchsiaX64: Int8(),
-//   Abi.fuchsiaRiscv64: Uint8(),
-//   Abi.iosArm: Int8(),
-//   Abi.iosArm64: Int8(),
-//   Abi.iosX64: Int8(),
-//   Abi.linuxArm: Uint8(),
-//   Abi.linuxArm64: Uint8(),
-//   Abi.linuxIA32: Int8(),
-//   Abi.linuxX64: Int8(),
-//   Abi.linuxRiscv32: Uint8(),
-//   Abi.linuxRiscv64: Uint8(),
-//   Abi.macosArm64: Int8(),
-//   Abi.macosX64: Int8(),
-//   Abi.windowsArm64: Int8(),
-//   Abi.windowsIA32: Int8(),
-//   Abi.windowsX64: Int8(),
-// })
-// final class FooChar extends AbiSpecificInteger {
-//   const FooChar();
-// }
-
-// @pragma("wasm:export")
-// void loadFlutterAsset(Pointer<Char> path, Pointer<Void> context) async {
-//   final codeUnits = path.cast<Uint8>();
-//   var length = 0;
-//   var bytes = <int>[];
-//   int i = 0;
-//   while (true) {
-//     var val = flutter_filament_web_get(path, i);
-//     i++;
-//     if (val != 0) {
-//       bytes.add(val);
-//     } else {
-//       break;
-//     }
-//   }
-//   var pathString = utf8.decode(bytes);
-
-//   var bd = await rootBundle.load(pathString);
-
-//   var dataPtr = flutter_filament_web_allocate(bd.lengthInBytes).cast<Char>();
-
-//   for (int i = 0; i < bd.lengthInBytes; i++) {
-//     flutter_filament_web_set(dataPtr, i, bd.getUint8(i));
-//   }
-//   flutter_filament_web_load_resource_callback(
-//       dataPtr.cast<Void>(), bd.lengthInBytes, context);
-// }
-
 /// A web implementation of the FlutterFilamentPlatform of the FlutterFilament plugin.
 class FlutterFilamentPluginWeb {
-  // final _dummy = FooChar();
+  static final _isInitialized = Completer<bool>();
+  static Future get isInitialized => _isInitialized.future;
 
-  FlutterFilamentPluginWeb() {}
   static void registerWith(Registrar registrar) async {
     final MethodChannel channel = MethodChannel("app.polyvox.filament/event",
         const StandardMethodCodec(), registrar.messenger);
     final FlutterFilamentPluginWeb instance = FlutterFilamentPluginWeb();
     channel.setMethodCallHandler(instance.handleMethodCall);
     await initializeBindings(rootBundle);
+    _isInitialized.complete(true);
   }
 
   Future handleMethodCall(MethodCall call) async {
